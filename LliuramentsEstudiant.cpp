083a9d2c6067c85251a12826fb24d28162a4f4bb
Toni Zarzoso <tonizrives@gmail.com>
2023-08-24 14:14:03 +0200

    Addition of function eliminaTramesa() in class LliuramentEstudiant. It allows a student to eliminate a Tramesa from its corrresponding m_trameses array, by specifying the date of the tramesa.

--- LliuramentsEstudiant.cpp
+++ LliuramentsEstudiant.cpp
@@ -7,3 +7,42 @@ void LliuramentsEstudiant::afegeixTramesa(const string& fitxer, const string& da
 
 	return;
 }
+
+
+bool LliuramentsEstudiant::eliminaTramesa(const string& data)
+{
+	bool found = false;
+
+	forward_list<Tramesa>::iterator it = m_trameses.begin(), prev = m_trameses.before_begin();
+
+	while(!found && it != m_trameses.end())
+	{
+		if(it->m_data == data)
+		{
+			found = true;
+			m_trameses.erase_after(prev);
+		}
+		else
+		{
+			it++;
+			prev++;
+		}
+	}
+
+	return found;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
