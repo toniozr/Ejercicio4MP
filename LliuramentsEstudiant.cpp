0751ead5ed631a8df3caafd71b4c67c8749779b8
Toni Zarzoso <tonizrives@gmail.com>
2023-08-24 14:11:49 +0200

    Addition of function afegeixTramesa() in class LliuramentEstudiant. It allows a student to add a Tramesa by specifying a file and a date

--- LliuramentsEstudiant.cpp
+++ LliuramentsEstudiant.cpp
@@ -1,1 +1,9 @@
 #include "LliuramentsEstudiant.h"
+
+void LliuramentsEstudiant::afegeixTramesa(const string& fitxer, const string& data)
+{
+	Tramesa novaTramesa(fitxer, data);
+	m_trameses.push_front(novaTramesa);
+
+	return;
+}
