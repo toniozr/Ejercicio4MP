37a3d99b9616886ac2b8983ff7e4173ad8acb933
Toni Zarzoso <tonizrives@gmail.com>
2023-08-24 18:53:43 +0200

    Addition of function: inicialitzaEstudiants() which allows to initialize the m_lliuraments array that belongs to class Exercici from a text file

--- Exercici.cpp
+++ Exercici.cpp
@@ -1,4 +1,30 @@
 #include "Exercici.h"
 #include <fstream>
+#include <iostream>
 using namespace std;
+
+
+void Exercici::inicialitzaEstudiants(const string& fitxerEstudiants)
+{
+	ifstream fitxer;
+	fitxer.open(fitxerEstudiants);
+	if(!fitxer.is_open())
+	{
+		cout << "Fitxer: '" << fitxerEstudiants << "' no s'ha obert correctament a la funció inicialitzaEstudiants" << endl;
+	}
+	else
+	{
+		fitxer >> m_nEstudiants;
+		m_lliuraments = new LliuramentsEstudiant[m_nEstudiants];
+		string niu;
+		int i = 0;
+		while(!fitxer.eof() && i < m_nEstudiants)
+		{
+			fitxer >> niu;
+			m_lliuraments[i].setNiu(niu);
+			i++;
+		}
+	}
+}
+
 
